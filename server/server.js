c4672d1f6f9e080fcfe42788713c3c2de0fc25f6
Your Name <you@example.com>
2019-01-16 14:06:49 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const path=require('path');
 const express=require('express');
 var app=express();
 const publicPath=path.join(__dirname,'../public');
-
+const port=proess.env.PORT||300;
 app.use(express.static(publicPath));
 app.get('/',(req,res)=>
 {
    res.send('hello world');
 });
-app.listen(3000,()=>
+app.listen(port,()=>
 {
-    console.log('Listening on port 3000');
+    console.log(`Listening on port ${port}`);
 })
