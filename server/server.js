6e36fec603fb5a1efa570a185f46c6b9bbb3e0ab
Your Name <you@example.com>
2019-01-16 14:09:56 +0530

    'fw'

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path=require('path');
 const express=require('express');
 var app=express();
 const publicPath=path.join(__dirname,'../public');
-const port=proess.env.PORT||300;
+const port=process.env.PORT||3000;
 app.use(express.static(publicPath));
 app.get('/',(req,res)=>
 {
