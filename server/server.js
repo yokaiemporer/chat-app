b3cf417b10c28afba36debf85027c74c01f64376
Your Name <you@example.com>
2019-01-16 18:38:27 +0530

    'wd'

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const path=require('path');
 const express=require('express');
-var app=express();
+const http=require('http');
+const socketIO=require('socket.io');
+
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT||3000;
+
+// app.get('/',(req,res)=>
+// {
+//    res.send('hello world');
+// });
+var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
+
 app.use(express.static(publicPath));
-app.get('/',(req,res)=>
-{
-   res.send('hello world');
+
+io.on('connection',(socket)=>{
+console.log('new user connected');
+socket.on('disconnect',()=>{
+console.log('user wa sdisconnected');
 });
-app.listen(port,()=>
+});
+server.listen(port,()=>
 {
     console.log(`Listening on port ${port}`);
 })
